d5388d4cdcec2c7ad88050190c5b1b703ceec748
Luca Rizzi <lrizzi@keck.hawaii.edu>
2018-04-13 10:14:33 -1000

    Bug (using float to save object??)

--- KCWI_Exposure.py
+++ KCWI_Exposure.py
@@ -135,7 +135,7 @@ class MyWindow(QWidget):
         except:
             kroot = ""
         if object:
-            cmdline = os.path.join(kroot, 'rel', 'default', 'bin', 'object %d' % float(object))
+            cmdline = os.path.join(kroot, 'rel', 'default', 'bin', 'object %d' % str(object))
             if self.runMode is not 'debug':
                 p = subprocess.Popen(cmdline, stdout = subprocess.PIPE,stderr = subprocess.PIPE, shell=True)
                 output, errors = p.communicate()
